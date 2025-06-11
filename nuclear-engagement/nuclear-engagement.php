1d1612678b93ec0d8fd4b2cd5bb7f95a15de8d8f
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-11 12:49:16 +0800

    Update PHP requirement to 7.4

--- nuclear-engagement/nuclear-engagement.php
+++ nuclear-engagement/nuclear-engagement.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Author:            Stefano Lodola
  * Requires at least: 5.6
  * Tested up to:      6.8
- * Requires PHP:      7.3
+ * Requires PHP:      7.4
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       nuclear-engagement
