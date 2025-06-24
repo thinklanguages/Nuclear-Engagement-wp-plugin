8ee317fed1b614b4e10083d93ba2a12533c2acff
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-24 13:35:16 +0800

    Add debounce to inventory cache

---
+++ tests/InventoryCacheTest.php
@@ -0,0 +1,53 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use NuclearEngagement\InventoryCache;
+
+// ----------------------------------------------
+// WordPress cache stubs
+// ----------------------------------------------
+
+$GLOBALS['wp_cache'] = [];
+$GLOBALS['flush_count'] = 0;
+
+if ( ! function_exists( 'wp_cache_get' ) ) {
+    function wp_cache_get( $key, $group = '', $force = false, &$found = null ) {
+        $found = isset( $GLOBALS['wp_cache'][ $group ][ $key ] );
+        return $GLOBALS['wp_cache'][ $group ][ $key ] ?? false;
+    }
+}
+if ( ! function_exists( 'wp_cache_set' ) ) {
+    function wp_cache_set( $key, $value, $group = '', $ttl = 0 ) {
+        $GLOBALS['wp_cache'][ $group ][ $key ] = $value;
+    }
+}
+if ( ! function_exists( 'wp_cache_delete' ) ) {
+    function wp_cache_delete( $key, $group = '' ) {
+        $GLOBALS['flush_count']++;
+        unset( $GLOBALS['wp_cache'][ $group ][ $key ] );
+    }
+}
+if ( ! function_exists( 'wp_cache_flush_group' ) ) {
+    function wp_cache_flush_group( $group ) {
+        unset( $GLOBALS['wp_cache'][ $group ] );
+    }
+}
+
+class InventoryCacheTest extends TestCase {
+    protected function setUp(): void {
+        global $wp_cache, $flush_count;
+        $wp_cache = [];
+        $flush_count = 0;
+    }
+
+    public function test_clear_is_debounced() {
+        InventoryCache::clear();
+        $this->assertSame( 1, $GLOBALS['flush_count'] );
+
+        InventoryCache::clear();
+        $this->assertSame( 1, $GLOBALS['flush_count'] );
+
+        usleep( ( InventoryCache::CLEAR_DEBOUNCE + 1 ) * 1000000 );
+        InventoryCache::clear();
+        $this->assertSame( 2, $GLOBALS['flush_count'] );
+    }
+}
