2bf4050e60a054a84260393346d332057d100f41
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-26 15:07:26 +0800

    Add sample C# program

---
+++ NuclearSample.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NuclearEngagement
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nuclear Engagement plugin sample");
+        }
+    }
+}
