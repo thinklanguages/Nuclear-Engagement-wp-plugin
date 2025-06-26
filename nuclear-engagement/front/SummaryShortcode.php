78a0fb61a22848cef467f49c9ca155c2856e8a50
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-26 14:09:38 +0800

    Add wrapper SummaryShortcode class

---
+++ nuclear-engagement/front/SummaryShortcode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Backwards compatibility wrapper for moved class.
+ */
+
+declare(strict_types=1);
+
+namespace NuclearEngagement\Front;
+
+use NuclearEngagement\Modules\Summary\Nuclen_Summary_Shortcode as ModuleSummaryShortcode;
+
+class SummaryShortcode extends ModuleSummaryShortcode {}
