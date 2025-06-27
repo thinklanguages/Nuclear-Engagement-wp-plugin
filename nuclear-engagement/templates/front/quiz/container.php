e9612a3a13b654ae06e0b4ad8b41eed127a3c79f
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-27 14:07:50 +0800

    Refactor quiz view to use template partials

---
+++ nuclear-engagement/templates/front/quiz/container.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<section id="nuclen-quiz-container" class="nuclen-quiz">
+<?php echo $start_message; ?>
+<?php echo $title; ?>
+<?php echo $progress_bar; ?>
+<?php echo $question_container; ?>
+<?php echo $answers_container; ?>
+<?php echo $result_container; ?>
+<?php echo $explanation_container; ?>
+<?php echo $next_button; ?>
+<?php echo $final_result_container; ?>
+</section>
---
+++ nuclear-engagement/templates/front/quiz/start-message.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+?>
+<div id="nuclen-quiz-start-message" class="nuclen-fg">
+<?php echo shortcode_unautop( $html_before ); ?>
+</div>
