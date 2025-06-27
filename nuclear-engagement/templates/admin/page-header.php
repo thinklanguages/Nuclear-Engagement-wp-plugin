b13f76ff1b0b1aa5fcedc6ed2545d8a30c54af8e
Stefano Lodola <117428905+thinklanguages@users.noreply.github.com>
2025-06-27 19:42:29 +0800

    Fix undefined image URL warning

--- nuclear-engagement/templates/admin/page-header.php
+++ nuclear-engagement/templates/admin/page-header.php
@@ -1,19 +1,24 @@
 <?php
 declare(strict_types=1);
+
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
+
+
 /**
- * Admin page header template.
- *
- * Variables:
- *   - $image_url (string) URL to the logo image.
- *
- * @package NuclearEngagement\Admin
- */
+	* Admin page header template.
+	*
+	* Variables:
+	*   - $image_url (string) URL to the logo image.
+	*
+	* @package NuclearEngagement\Admin
+	*/
+
+$image_url = isset( $image_url ) ? (string) $image_url : '';
 ?>
 <div id="nuclen-page-header">
-    <img height="40" width="40" src="<?php echo esc_url( $image_url ); ?>" alt="<?php esc_attr_e( 'Nuclear Engagement Logo', 'nuclear-engagement' ); ?>" />
-    <p><b><?php esc_html_e( 'NUCLEAR ENGAGEMENT', 'nuclear-engagement' ); ?></b></p>
+	<img height="40" width="40" src="<?php echo esc_url( $image_url ); ?>" alt="<?php esc_attr_e( 'Nuclear Engagement Logo', 'nuclear-engagement' ); ?>" />
+	<p><b><?php esc_html_e( 'NUCLEAR ENGAGEMENT', 'nuclear-engagement' ); ?></b></p>
 </div>
 
